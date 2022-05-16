6b4da950f2dc6fe8815100e0165267eac1593294
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-05-16 12:03:24 +0200

    sumSTrings

---
+++ sumStrings.js
@@ -0,0 +1,4 @@
+// Given the string representations of two integers, return the string representation of the sum of those integers.
+function sumStrings(a, b) {
+  return (BigInt(a) + BigInt(b)).toString()
+}
